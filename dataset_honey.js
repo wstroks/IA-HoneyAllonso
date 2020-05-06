0d5029cd34f640c80d7dc60c2ef019b1f8ec4e2b
Washington Pagotto Batista <wstroks@hotmail.com>
2020-05-06 16:55:40 -0300

    Initial commit

---
+++ dataset_honey.js
@@ -0,0 +1,319 @@
+let dataseet = []
+        for (var i = 0; i < 3000; i++) {
+
+
+            dataseet.push({
+                p1: Math.floor(Math.random() * 2),
+                p2: Math.floor(Math.random() * 2),
+                p3: Math.floor(Math.random() * 2),
+                p4: Math.floor(Math.random() * 2),
+                p5: Math.floor(Math.random() * 2),
+                p6: Math.floor(Math.random() * 2),
+                p7: Math.floor(Math.random() * 2),
+                p8: Math.floor(Math.random() * 2),
+                p9: Math.floor(Math.random() * 2),
+                p10: Math.floor(Math.random() * 2),
+                p11: Math.floor(Math.random() * 2),
+                p12: Math.floor(Math.random() * 2),
+                p13: Math.floor(Math.random() * 2),
+                p14: Math.floor(Math.random() * 2),
+                p15: Math.floor(Math.random() * 2),
+                p16: Math.floor(Math.random() * 2),
+                p17: Math.floor(Math.random() * 2),
+                p18: Math.floor(Math.random() * 2),
+                p19: Math.floor(Math.random() * 2),
+                p20: Math.floor(Math.random() * 2),
+                p21: Math.floor(Math.random() * 2),
+                p22: Math.floor(Math.random() * 2),
+                p23: Math.floor(Math.random() * 2),
+                p24: Math.floor(Math.random() * 2),
+                p25: Math.floor(Math.random() * 2),
+
+                p26: Math.floor(Math.random() * 2),
+                p27: Math.floor(Math.random() * 2),
+                p28: Math.floor(Math.random() * 2),
+                p29: Math.floor(Math.random() * 2),
+                p30: Math.floor(Math.random() * 2),
+
+                p31: Math.floor(Math.random() * 2),
+                p32: Math.floor(Math.random() * 2),
+                p33: Math.floor(Math.random() * 2),
+                p34: Math.floor(Math.random() * 2),
+
+                p35: Math.floor(Math.random() * 2),
+                p36: Math.floor(Math.random() * 2),
+                p37: Math.floor(Math.random() * 2),
+                p38: Math.floor(Math.random() * 2),
+                p39: Math.floor(Math.random() * 2),
+                p40: Math.floor(Math.random() * 2),
+                p41: Math.floor(Math.random() * 2),
+                p42: Math.floor(Math.random() * 2),
+                p43: Math.floor(Math.random() * 2),
+                p44: Math.floor(Math.random() * 2),
+                p45: Math.floor(Math.random() * 2),
+                p46: Math.floor(Math.random() * 2),
+                p47: Math.floor(Math.random() * 2),
+                p48: Math.floor(Math.random() * 2),
+                p49: Math.floor(Math.random() * 2),
+
+                p50: Math.floor(Math.random() * 2),
+                p51: Math.floor(Math.random() * 2),
+                p52: Math.floor(Math.random() * 2),
+                p53: Math.floor(Math.random() * 2),
+                p54: Math.floor(Math.random() * 2),
+                p55: Math.floor(Math.random() * 2),
+                p56: Math.floor(Math.random() * 2),
+                p57: Math.floor(Math.random() * 2),
+                p58: Math.floor(Math.random() * 2),
+                p59: Math.floor(Math.random() * 2),
+                p60: Math.floor(Math.random() * 2),
+                p61: Math.floor(Math.random() * 2),
+                p62: Math.floor(Math.random() * 2),
+
+                p63: Math.floor(Math.random() * 2),
+                p64: Math.floor(Math.random() * 2),
+                p65: Math.floor(Math.random() * 2),
+                p66: Math.floor(Math.random() * 2),
+                p67: Math.floor(Math.random() * 2),
+                p68: Math.floor(Math.random() * 2),
+                p69: Math.floor(Math.random() * 2),
+                p70: Math.floor(Math.random() * 2),
+                p71: Math.floor(Math.random() * 2),
+                p72: Math.floor(Math.random() * 2),
+                p73: Math.floor(Math.random() * 2),
+                p74: Math.floor(Math.random() * 2),
+                p75: Math.floor(Math.random() * 2),
+                p76: Math.floor(Math.random() * 2),
+
+                p77: Math.floor(Math.random() * 2),
+                p78: Math.floor(Math.random() * 2),
+                p79: Math.floor(Math.random() * 2),
+                p80: Math.floor(Math.random() * 2),
+
+            });
+        }
+        var label_allonso = [];
+        for (var y = 0; y < 3000; y++) {
+            var ativo = 0;
+            var reflexivo = 0;
+            var teorico = 0;
+            var pragmatico = 0;
+
+            if (dataseet[y].p3 == 1) {
+                ativo++;
+            }
+            if (dataseet[y].p5 == 1) {
+                ativo++;
+            }
+            if (dataseet[y].p7 == 1) {
+                ativo++;
+            }
+            if (dataseet[y].p9 == 1) {
+                ativo++;
+            }
+            if (dataseet[y].p13 == 1) {
+                ativo++;
+            }
+            if (dataseet[y].p20 == 1) {
+                ativo++;
+            }
+            if (dataseet[y].p26 == 1) {
+                ativo++;
+            }
+            if (dataseet[y].p27 == 1) {
+                ativo++;
+            }
+            if (dataseet[y].p35 == 1) {
+                ativo++;
+            }
+            if (dataseet[y].p37 == 1) {
+                ativo++;
+            } if (dataseet[y].p41 == 1) {
+                ativo++;
+            } if (dataseet[y].p43 == 1) {
+                ativo++;
+            } if (dataseet[y].p46 == 1) {
+                ativo++;
+            } if (dataseet[y].p48 == 1) {
+                ativo++;
+            } if (dataseet[y].p51 == 1) {
+                ativo++;
+            } if (dataseet[y].p61 == 1) {
+                ativo++;
+            } if (dataseet[y].p67 == 1) {
+                ativo++;
+            } if (dataseet[y].p74 == 1) {
+                ativo++;
+            } if (dataseet[y].p75 == 1) {
+                ativo++;
+            } if (dataseet[y].p77 == 1) {
+                ativo++;
+            } if (dataseet[y].p10 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p16 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p18 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p19 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p28 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p31 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p32 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p34 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p36 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p39 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p42 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p44 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p49 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p55 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p58 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p63 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p65 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p69 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p70 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p79 == 1) {
+                reflexivo++;
+            }
+            if (dataseet[y].p2 == 1) {
+                teorico++;
+            }
+            if (dataseet[y].p4 == 1) {
+                teorico++;
+            }
+            if (dataseet[y].p6 == 1) {
+                teorico++;
+            }
+            if (dataseet[y].p11 == 1) {
+                teorico++;
+            }
+            if (dataseet[y].p15 == 1) {
+                teorico++;
+            }
+            if (dataseet[y].p17 == 1) {
+                teorico++;
+            } if (dataseet[y].p21 == 1) {
+                teorico++;
+            } if (dataseet[y].p23 == 1) {
+                teorico++;
+            } if (dataseet[y].p25 == 1) {
+                teorico++;
+            } if (dataseet[y].p29 == 1) {
+                teorico++;
+            } if (dataseet[y].p33 == 1) {
+                teorico++;
+            } if (dataseet[y].p45 == 1) {
+                teorico++;
+            } if (dataseet[y].p50 == 1) {
+                teorico++;
+            } if (dataseet[y].p54 == 1) {
+                teorico++;
+            } if (dataseet[y].p60 == 1) {
+                teorico++;
+            } if (dataseet[y].p64 == 1) {
+                teorico++;
+            } if (dataseet[y].p66 == 1) {
+                teorico++;
+            } if (dataseet[y].p71 == 1) {
+                teorico++;
+            } if (dataseet[y].p78 == 1) {
+                teorico++;
+            } if (dataseet[y].p80 == 1) {
+                teorico++;
+            } if (dataseet[y].p1 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p8 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p12 == 1) {
+                pragmatico++;
+            }
+            if (dataseet[y].p14 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p22 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p24 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p30 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p38 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p40 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p47 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p52 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p53 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p56 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p57 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p59 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p62 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p68 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p72 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p73 == 1) {
+                pragmatico++;
+            } if (dataseet[y].p76 == 1) {
+                pragmatico++;
+            }
+            console.log("Agora " + pragmatico + " teorico " + teorico + " reflexivo " + reflexivo, " ativo " + ativo);
+            if (pragmatico >= reflexivo && pragmatico >= ativo && pragmatico >= teorico) {
+                label_allonso.push({ "tipo_aprendizagem": "Pragmatico" })
+            } else if (reflexivo >= pragmatico && reflexivo >= ativo && reflexivo >= teorico) {
+                label_allonso.push({ "tipo_aprendizagem": "Reflexivo" })
+            } else if (ativo >= pragmatico && ativo >= reflexivo && ativo >= teorico) {
+                label_allonso.push({ "tipo_aprendizagem": "Ativo" })
+            } else if (teorico >= pragmatico && teorico >= reflexivo && teorico >= ativo) {
+                label_allonso.push({ "tipo_aprendizagem": "Teorico" })
+            }
+
+
+        }
+        const devtoListTrimmed = dataseet.filter(n => n != undefined)
+        fs.writeFile('honey-allonso.json',
+            JSON.stringify(devtoListTrimmed, null, 4),
+            (err) => console.log('File successfully written!'))
+
+        const devtoListTrimmed1 = label_allonso.filter(n => n != undefined)
+        fs.writeFile('label-honey-allonso.json',
+            JSON.stringify(devtoListTrimmed1, null, 4),
+            (err) => console.log('File successfully written!'))
